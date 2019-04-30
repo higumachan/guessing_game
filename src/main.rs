89e20a97dd90c11ccd4d5c67c5bb7edf0432df9e
higumachan <harekumo792154@gmail.com>
2019-04-30 17:26:33 +0900

    入力の位置を変える。

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Please input your guess");
 
     loop {
+        println!("Please input your guess");
+
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess).expect("Failed to read line");
